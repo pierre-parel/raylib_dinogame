79408f0b915ac112205a3a29339014e6b85f0abe
pierre-parel <pierre_parel@dlsu.edu.ph>
2024-04-27 18:09:11 +0800

    Add score, restart, and end screen

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include <cmath>
+#include <iostream>
 #include <vector>
 
 // Enum
@@ -7,7 +9,7 @@ enum ObstacleType { GROUND, AIR };
 
 class Player {
 public:
-  Vector2 pos;
+  Rectangle rect;
   float speed;
   bool canJump;
 };
@@ -15,21 +17,23 @@ public:
 class Obstacle {
 public:
   ObstacleType obstacleType = GROUND;
-  Vector2 pos{0, 200};
+  Rectangle rect;
 };
 
 // Generic game variables
 static const int WINDOW_WIDTH = 800;
 static const int WINDOW_HEIGHT = 300;
 
-static const float PLAYER_JMP_SPEED = 350.0f;
+static const float PLAYER_JMP_SPEED = 450.0f;
 static const float GRAVITY = 900;
-static const int MAX_OBSTACLES = 100;
+static const int MAX_OBSTACLES = 500;
 
 // TODO: Change whenever testing specific parts (Default: TITLE)
 static int currentScreen = GAMEPLAY;
 static bool exitGame = false;
+static bool isAlive = true;
 static int optionSelect = 0;
+static int score = 0;
 
 static Player player{};
 static std::vector<Obstacle> obstacles;
@@ -37,6 +41,7 @@ static std::vector<Vector2> obstaclePos;
 
 static int obstacleSpeed;
 static float deltaTime;
+static float lastUpdateTime;
 
 static RenderTexture2D screenTarget;
 static Texture2D player_atlas;
@@ -50,6 +55,8 @@ void UpdateObstacles();
 void DrawGame();
 
 int main() {
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dino Run");
+  SetTargetFPS(60);
   InitGame();
 
   while (!WindowShouldClose() && !exitGame) {
@@ -63,12 +70,19 @@ int main() {
 }
 
 void InitGame() {
-  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dino Run");
-  SetTargetFPS(60);
   GenerateObstacles();
 
+  currentScreen = GAMEPLAY;
+  exitGame = false;
+  isAlive = true;
+  optionSelect = 0;
+  score = 0;
   player.canJump = true;
-  player.pos.y = 200;
+  player.rect.y = 200;
+  player.rect.x = 60;
+  player.speed = 0;
+  player.rect.width = 60;
+  player.rect.height = 60;
 
   screenTarget = LoadRenderTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
   SetTextureFilter(screenTarget.texture, TEXTURE_FILTER_BILINEAR);
@@ -79,21 +93,25 @@ void InitGame() {
 void GenerateObstacles() {
   obstacles.reserve(MAX_OBSTACLES);
   obstaclePos.reserve(MAX_OBSTACLES);
-  obstacleSpeed = 5;
+  obstacleSpeed = 8;
 
   for (int i = 0; i < MAX_OBSTACLES; i++) {
-    obstaclePos[i].x = 400 + GetRandomValue(350, 600) * i;
-    obstaclePos[i].y = 200;
+    obstaclePos[i].x = 700 + GetRandomValue(600, 800) * i;
+    obstaclePos[i].y = 210;
   }
 
   for (int i = 0; i < MAX_OBSTACLES; i++) {
-    obstacles[i].pos.x = obstaclePos[i].x;
-    obstacles[i].pos.y = obstaclePos[i].y;
+    obstacles[i].rect.x = obstaclePos[i].x;
+    obstacles[i].rect.y = obstaclePos[i].y;
+    obstacles[i].rect.width = GetRandomValue(5, 10) * 10;
+    obstacles[i].rect.height = 50;
   }
 }
 
 void UpdateGame() {
   deltaTime = GetFrameTime();
+  lastUpdateTime = 0;
+
   switch (currentScreen) {
   case TITLE: {
     if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) {
@@ -123,33 +141,59 @@ void UpdateGame() {
   } break;
 
   case GAMEPLAY: {
-    UpdateObstacles();
-    UpdatePlayer();
+    if (isAlive) {
+      double currentTime = GetTime();
+
+      if (currentTime - lastUpdateTime >= 0.1) {
+        score++;
+      }
+
+      UpdateObstacles();
+      UpdatePlayer();
+    } else {
+      currentScreen = ENDING;
+      break;
+    }
   } break;
 
   case ENDING: {
+    DrawText("Game Over", WINDOW_WIDTH / 2 - 180, WINDOW_HEIGHT / 2, 20,
+             RAYWHITE);
+    DrawText(TextFormat("Score: %4i", score), WINDOW_WIDTH / 2 - 220,
+             WINDOW_HEIGHT / 2 + 20, 20, RAYWHITE);
+
+    if (IsKeyPressed(KEY_R)) {
+      currentScreen = GAMEPLAY;
+      InitGame();
+    }
   } break;
   }
 }
 
 void UpdatePlayer() {
-  // TODO: Collision detection
   if ((IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_UP)) && player.canJump) {
     player.speed = -PLAYER_JMP_SPEED;
     player.canJump = false;
   }
 
-  player.pos.y += player.speed * deltaTime;
+  // TODO: Collision detection
+  for (int i = 0; i < MAX_OBSTACLES; i++) {
+    if (CheckCollisionRecs(player.rect, obstacles[i].rect)) {
+      isAlive = false;
+    }
+  }
+
+  player.rect.y += player.speed * deltaTime;
   player.speed += GRAVITY * deltaTime;
-  if (player.pos.y >= 200) {
+  if (player.rect.y >= 200) {
     player.canJump = true;
-    player.pos.y = 200;
+    player.rect.y = 200;
   }
 }
 
 void UpdateObstacles() {
   for (int i = 0; i < MAX_OBSTACLES; i++) {
-    obstacles[i].pos.x -= obstacleSpeed;
+    obstacles[i].rect.x -= obstacleSpeed;
   }
 }
 
@@ -169,10 +213,15 @@ void DrawGame() {
   case GAMEPLAY: {
     // TODO: Sprite animation
     DrawTexturePro(player_atlas, Rectangle{0, 0, 24, 24},
-                   Rectangle{player.pos.x, player.pos.y, 60, 60},
-                   Vector2{12, 12}, 0.0f, RAYWHITE);
+                   Rectangle{player.rect.x, player.rect.y, player.rect.width,
+                             player.rect.height},
+                   Vector2{0, 0}, 0.0f, RAYWHITE);
+    DrawRectangleLinesEx(player.rect, 2.0f, RED);
+
     for (int i = 0; i < MAX_OBSTACLES; i++) {
-      DrawRectangle(obstacles[i].pos.x, obstacles[i].pos.y, 50, 50, RED);
+      DrawRectangle(obstacles[i].rect.x, obstacles[i].rect.y,
+                    obstacles[i].rect.width, obstacles[i].rect.height, RED);
+      DrawRectangleLinesEx(obstacles[i].rect, 2.0f, BLUE);
     }
   } break;
 
